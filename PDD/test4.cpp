d8c5c8194bf1f6da3cf411cb25616654e9538d0a
Freshman02 <2287974460@qq.com>
2025-03-12 12:21:06 +0800

    20250312-1

--- PDD/test4.cpp
+++ PDD/test4.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+  int n = 3;
+  int *a = new int[n]{1, 2};
 
-int main()
-{
-    int n = 3;
-    int *a = new int[n]{1,2};
+  for (int i = 0; i < n; ++i)
+    cout << a[i] << " ";
 
-    for (int i = 0; i < n; ++i)
-        cout << a[i] << " ";
-
-    return 0;
-} 
-
-
-
-
-
+  return 0;
+}
